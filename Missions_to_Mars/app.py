cd8387a0236d28de2afd5dbcf754d85e4ad53d58
Brooke Fish <brookecrofts1@gmail.com>
2020-09-17 14:46:14 -0600

    Code Thurs, need mongobd fix

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,31 +6,31 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_data"
 mongo = PyMongo(app)
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    mars = mongo.db.mars.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=mars)
+    return render_template("index.html", mars_data=mars_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    # mars = mongo.db.mars
+    mars_data = mongo.db.mars_data
+
     # Run the scrape function
-    mars_data = scrape_mars.scrape_all()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars.update({}, mars_data, upsert=True)
+    # mars_data.replace_one({}, mars_data, upsert=True)
+    mars_data.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return "Scraping Updated!"
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,109 +1,81 @@
 from splinter import Browser
-from bs4 import BeautifulSoup
-import pandas as pd
-import datetime as dt   
+from bs4 import BeautifulSoup as bs
+import pandas as pd  
 import time
+import pymongo
 import re
-
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {'executable_path': 'chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser('chrome', **executable_path, headless=False)
+    
 
-def scrape_info():
-    news_title, news_p = mars_news(browser)
-    # Store data in a dictionary
-    mars_data = {
-        "news_title": news_title,
-        "news_p": news_p,
-        "featured_image": featured_image,
-        "mars_facts": mars_facts,
-        "hemispheres": hemispheres
+def scrape():
+    browser = init_browser()
 
-    }
-    # Close the browser after scraping
-    browser.quit()
-    # Return results
-    return mars_data
-
-def mars_news(browser):
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(url)
 
     html = browser.html
-    news_soup = BeautifulSoup(html, "html.parser")
+    soup = bs(html, "html.parser")
 
-    try:
-        news = soup.find('div', class_="content_title").text
-        news_title = news.strip('\n')
-        news_2 = soup.find('div', class_="rollover_description_inner").text
-        news_p = news_2.strip('\n')
-        # "div", class_="article_teaser_body").get
-    except AttributeError:
-        return None, None
-    
-    return news_title, news_p
+    title_header = soup.find('div', class_='list_text')
+    news_title = title_header.find('div', class_="content_title").text
+    news_p = title_header.find('div', class_="article_teaser_body").text
 
-def featured_image(browser):
-    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    browser.visit(url)
+    mars_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(mars_url)
 
     #Find full size image after click Full image button
-    full_image = browser.find_by_id("full_image")
-    full_image.click()
+    browser.click_link_by_partial_text("FULL IMAGE")
+    browser.click_link_by_partial_text("more info")
 
     html = browser.html
-    img_soup = BeautifulSoup(html, "html.parser")
+    soup = bs(html, 'html.parser')
 
-    img = img_soup.select_one("figure.lede a img")
-
-    try: 
-        img_url_relative_path = img.get("src")
-
-    except AttributeError:
-        return None
+        #Find image
+    img_url_relative_path = soup.find('figure', class_='lede').a['href']
 
     #use base url to create absolute url
     featured_img_url = f"https://www.jpl.nasa.gov{img_url_relative_path}"
 
-    return featured_img_url
 
-def mars_facts:
-    url = 'https://space-facts.com/mars/'
-    tables = pd.read_html(url)
+    table_url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(table_url)
+    mars_df = tables[2]
 
-    try:
-    nasa_df = tables[2]
+    html_table = mars_df.to_html()
 
-    except AttributeError:
-        return None
-
-    return nasa_df
-
-def hemispheres(browser):
-
-    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-
-    browser.visit(url)
+    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemisphere_url)
+    time.sleep(1)
+    html = browser.html
+    soup = bs(html, "html.parser")
 
     hemisphere_image_urls = []
-    for i in range(4):
 
-        browser.find_by_css("a.product-item h3")[i].click()
+    baseimg_url="https://astrogeology.usgs.gov/"
+    # Soup object
+    hemispheres = soup.find_all('div', class_='item')
+    # Loop to get each title & url
+    for hemi in hemispheres:
+        title = hemi.find('h3').text
+        link = 'https://astrogeology.usgs.gov/' + hemi.find('a')['href']
+        browser.visit(link)
+        img_html = browser.html
+        img_soup = bs(img_html, "html.parser")
+        imgs_url = img_soup.find("img", class_="wide-image")["src"]
+        image_url = baseimg_url+imgs_url
+        hemisphere_image_urls.append({"title": title, "img_url": image_url})
 
-        hemi_data = scrape_hemisphere(browser.html)
-
-        #Append to the list
-        hemisphere_image_urls.append(hemi_data)
-
-        #Go back to previous page to get next one
-        browser.back()
-
-    return hemisphere_image_urls
-
-
-    # # BONUS: Find the src for the sloth image
-    # relative_image_path = soup.find_all('img')[2]["src"]
-    # sloth_img = url + relative_image_path
-
+   # Store data in a dictionary
+    mars_data = {
+        "news_title": news_title,
+        "news_p": news_p,
+        "featured_image": featured_img_url,
+        "mars_facts": html_table,
+        "hemispheres": hemisphere_image_urls
+    }
+    # Return results
+    return mars_data
